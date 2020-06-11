677587fc5386f8db8f9d73414a07e23462203043
Connor <cfech44@gmail.com>
2020-06-11 17:06:16 -0400

    addding functionalit for operator and number selectors

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-console.log("hello")
 
-    
-    
-    
+var firstNumber = ''
+var secondNumber = ""
+var operator = ""
+var result = 0
+var numberSelector = true
+var operatorSelector = false
+
+//number selectors 
+$(".number").on("click", function () {
+    console.log("hello")
+
+    if (numberSelector === false) {
+        firstNumber = firstNumber + $(this).val()
+        console.log("firstNumber", firstNumber)
+        $("#first-number").text(firstNumber)
+
+    } else {
+        secondNumber = secondNumber + $(this).val()
+        console.log("secondNumber", secondNumber)
+        $("#second-number").text(secondNumber)
+
+    }
+})
+
+// operator selector 
+$(".operator").on("click", function () {
+    if (operatorSelector === false) {
+        operatorSelector = true
+        console.log("operatorSelector", operatorSelector)
+
+        console.log($(this).val())
+
+        operator = $(this).val()
+        console.log("operator", operator)
+        numberSelector = false
+
+
+    } else {
+        console.log("operator selected")
+    }
+})
+
