d9d28a42a3a5d5616b4705e0662009e53550c203
Connor <cfech44@gmail.com>
2020-06-11 17:19:48 -0400

    updated calculate funciton and validation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var firstNumber = ''
 var secondNumber = ""
 var operator = ""
 var result = 0
-var numberSelector = true
+var numberSelector = false
 var operatorSelector = false
 
 //number selectors 
@@ -11,6 +11,9 @@ $(".number").on("click", function () {
     console.log("hello")
 
     if (numberSelector === false) {
+
+        
+
         firstNumber = firstNumber + $(this).val()
         console.log("firstNumber", firstNumber)
         $("#first-number").text(firstNumber)
@@ -27,13 +30,14 @@ $(".number").on("click", function () {
 $(".operator").on("click", function () {
     if (operatorSelector === false) {
         operatorSelector = true
+        numberSelector = true
         console.log("operatorSelector", operatorSelector)
 
         console.log($(this).val())
 
         operator = $(this).val()
         console.log("operator", operator)
-        numberSelector = false
+ 
 
 
     } else {
@@ -41,3 +45,34 @@ $(".operator").on("click", function () {
     }
 })
 
+
+function calculate(){
+    console.log("hello")
+    console.log(operator)
+    switch(operator){
+        case("plus"):
+        return parseInt(firstNumber) + parseInt(secondNumber);
+        case("minus"):
+        return parseInt(firstNumber) - parseInt(secondNumber);
+        case("times"):
+        return parseInt(firstNumber) * parseInt(secondNumber);
+        case("divide"):
+        return parseInt(firstNumber) / parseInt(secondNumber);
+        case("power"):
+        return Math.pow(parseInt(firstNumber), parseInt(secondNumber));
+    }
+
+}
+
+
+
+// equals
+$(".equal").on("click", function(){
+    if(firstNumber !== "" && secondNumber !== "" && operator !== ""){
+ var answer =  calculate()
+ console.log("answer", answer)
+    }
+    else{
+        alert("please select a first number, second number and an operator fo the calculator to function correctly")
+    }
+})
